b55b45fa1ae411046f9e2ae5f3a4640183993f6f
akorzunin <akorzunin123@gmail.com>
2023-07-09 23:21:36 +0300

    fix(cmp): update lsp keymaps

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -70,7 +70,9 @@ local options = {
 
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<Up>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<Down>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
