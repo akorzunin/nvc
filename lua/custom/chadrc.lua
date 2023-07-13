4e4dff30fabbc0467b678ce2540cfdd4b0489d9a
akorzunin <akorzunin123@gmail.com>
2023-07-13 09:18:14 +0300

    fix: fixed statusline /# test

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -4,11 +4,18 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "rosepine",
-  theme_toggle = { "rosepine", "catppuccin",  },
+    theme = "rosepine",
+    theme_toggle = { "rosepine", "catppuccin",  },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+    hl_override = highlights.override,
+    hl_add = highlights.add,
+    statusline = {
+        theme = "default", -- default/vscode/vscode_colored/minimal
+        -- default/round/block/arrow (separators work only for "default" statusline theme;
+        -- round and block will work for the minimal theme only)
+        separator_style = "block",
+        overriden_modules = nil,
+    },
 }
 
 M.mappings = require "custom.mappings"
