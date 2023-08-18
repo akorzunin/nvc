5e3ce328e1c3a1b341bbe19ab11ecabfc2ab5083
akorzunin <akorzunin123@gmail.com>
2023-08-18 09:02:29 +0300

    fix(plugins): fix trouble plugin load

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -154,6 +154,7 @@ local plugins = {
     {
         "folke/trouble.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
+        event = "VeryLazy",
         opts = {
             -- your configuration comes here
             -- or leave it empty to use the default settings
