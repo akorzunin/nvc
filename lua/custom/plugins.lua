7e1fe06c635a95df491e109fd723452bca8db3e8
akorzunin <akorzunin123@gmail.com>
2023-10-23 15:09:51 +0300

    feat: add config override fro nvterm

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,29 +1,29 @@
 local plugins = {
     {
         "nvim-tree/nvim-web-devicons",
-        event = "VeryLazy"
+        event = "VeryLazy",
     },
     {
         "tpope/vim-fugitive",
-        event = "VeryLazy"
+        event = "VeryLazy",
     },
     {
         "mbbill/undotree",
-        event = "VeryLazy"
+        event = "VeryLazy",
     },
     -- DEBUGGER
     {
         "mfussenegger/nvim-dap",
         config = function(_, opts)
-            require("core.utils").load_mappings("dap")
-        end
+            require("core.utils").load_mappings "dap"
+        end,
     },
     {
         "rcarriga/nvim-dap-ui",
         dependencies = "mfussenegger/nvim-dap",
         config = function()
-            local dap = require("dap")
-            local dapui = require("dapui")
+            local dap = require "dap"
+            local dapui = require "dapui"
             dapui.setup()
             dap.listeners.after.event_initialized["dapui_config"] = function()
                 dapui.open()
@@ -34,20 +34,20 @@ local plugins = {
             dap.listeners.before.event_exited["dapui_config"] = function()
                 dapui.close()
             end
-        end
+        end,
     },
     {
         "theHamsta/nvim-dap-virtual-text",
         otps = {
-            enabled = true,                     -- enable this plugin (the default)
-            enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+            enabled = true, -- enable this plugin (the default)
+            enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
             highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-            highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-            show_stop_reason = true,            -- show stop reason when stopped for exceptions
-            commented = false,                  -- prefix virtual text with comment string
-            only_first_definition = true,       -- only show virtual text at first definition (if there are multiple)
-            all_references = false,             -- show virtual text on all all references of the variable (not only definitions)
-            clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
+            highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+            show_stop_reason = true, -- show stop reason when stopped for exceptions
+            commented = false, -- prefix virtual text with comment string
+            only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+            all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+            clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
             --- A callback that determines how a variable is displayed or whether it should be omitted
             --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
             --- @param buf number
@@ -55,41 +55,48 @@ local plugins = {
             --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
             --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
             --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
-            display_callback = function(variable, buf, stackframe, node, options)
-                if options.virt_text_pos == 'inline' then
-                    return ' = ' .. variable.value
+            display_callback = function(
+                variable,
+                buf,
+                stackframe,
+                node,
+                options
+            )
+                if options.virt_text_pos == "inline" then
+                    return " = " .. variable.value
                 else
-                    return variable.name .. ' = ' .. variable.value
+                    return variable.name .. " = " .. variable.value
                 end
             end,
             -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
-            virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
+            virt_text_pos = vim.fn.has "nvim-0.10" == 1 and "inline" or "eol",
             -- experimental features:
-            all_frames = false,     -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-            virt_lines = false,     -- show virtual lines instead of virtual text (will flicker!)
-            virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
+            all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+            virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+            virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
             -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-        }
+        },
     },
     {
         "nvim-telescope/telescope-dap.nvim",
     },
     {
         "leoluz/nvim-dap-go",
-        ft = "go"
+        ft = "go",
     },
     {
         "mfussenegger/nvim-dap-python",
         ft = "python",
         dependencies = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
         config = function(_, opts)
-            require("core.utils").load_mappings("dap_python")
-            local path = 'C:\\Users\\avkorz\\AppData\\Local\\Programs\\Python\\Python310\\python.exe'
-            local dap = require('dap')
+            require("core.utils").load_mappings "dap_python"
+            local path =
+                "C:\\Users\\avkorz\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"
+            local dap = require "dap"
             dap.configurations.python = {
                 {
-                    type = 'python',
-                    request = 'launch',
+                    type = "python",
+                    request = "launch",
                     name = "Launch file",
                     program = "${file}",
                     pythonPath = function()
@@ -99,7 +106,7 @@ local plugins = {
             }
             require("dap-python").setup(path)
             -- require("core.utils").load_mappings("dap_python")
-        end
+        end,
     },
     --
     {
@@ -115,16 +122,16 @@ local plugins = {
                 "ruff",
                 "ruff-lsp",
                 "pyright",
-                "gopls"
-            }
-        }
+                "gopls",
+            },
+        },
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
             require "plugins.configs.lspconfig"
             require "custom.configs.lspconfig"
-        end
+        end,
     },
     {
         "folke/trouble.nvim",
@@ -142,64 +149,68 @@ local plugins = {
                 information = "",
                 other = "",
             },
-        }
+        },
     },
     {
-        'rose-pine/neovim'
+        "rose-pine/neovim",
     },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         event = "VeryLazy",
         config = function()
             -- This is where you modify the settings for lsp-zero
             -- Note: autocompletion settings will not take effect
 
-            require('lsp-zero.settings').preset({})
-        end
+            require("lsp-zero.settings").preset {}
+        end,
     },
     {
-        'neovim/nvim-lspconfig',
-        cmd = 'LspInfo',
-        event = { 'BufReadPre', 'BufNewFile' },
-        dependencies = { { 'hrsh7th/cmp-nvim-lsp' }, { 'williamboman/mason-lspconfig.nvim' }, {
-            'williamboman/mason.nvim',
-            build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end
-        } },
+        "neovim/nvim-lspconfig",
+        cmd = "LspInfo",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "williamboman/mason-lspconfig.nvim" },
+            {
+                "williamboman/mason.nvim",
+                build = function()
+                    pcall(vim.cmd, "MasonUpdate")
+                end,
+            },
+        },
         config = function()
             -- This is where all the LSP shenanigans will live
 
-            local lsp = require('lsp-zero')
+            local lsp = require "lsp-zero"
 
             lsp.on_attach(function(client, bufnr)
-                lsp.default_keymaps({
-                    buffer = bufnr
-                })
+                lsp.default_keymaps {
+                    buffer = bufnr,
+                }
             end)
 
             -- (Optional) Configure lua language server for neovim
-            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+            require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
             lsp.setup()
-        end
+        end,
     },
     {
         "akorzunin/project.nvim",
         config = function()
             require("project_nvim").setup {
-                show_files = 'builtin',
+                show_files = "builtin",
             }
-            require("nvim-tree").setup({
+            require("nvim-tree").setup {
                 sync_root_with_cwd = true,
                 respect_buf_cwd = true,
                 update_focused_file = {
                     enable = true,
-                    update_root = true
+                    update_root = true,
                 },
-            })
-        end
+            }
+        end,
     },
     {
         "folke/todo-comments.nvim",
@@ -214,7 +225,7 @@ local plugins = {
             local tdc = require "todo-comments"
             tdc.opts = {}
             tdc.setup()
-        end
+        end,
     },
     {
         "equalsraf/neovim-gui-shim",
@@ -226,14 +237,18 @@ local plugins = {
             "neovim/nvim-lspconfig",
             "nvim-telescope/telescope.nvim",
             -- for DAP support
-            "mfussenegger/nvim-dap-python"
+            "mfussenegger/nvim-dap-python",
         },
         config = true,
-        keys = { {
-            "<leader>vs", "<cmd>:VenvSelect<cr>",
-            -- optional if you use a autocmd (see #🤖-Automate)
-            "<leader>vc", "<cmd>:VenvSelectCached<cr>"
-        } },
+        keys = {
+            {
+                "<leader>vs",
+                "<cmd>:VenvSelect<cr>",
+                -- optional if you use a autocmd (see #🤖-Automate)
+                "<leader>vc",
+                "<cmd>:VenvSelectCached<cr>",
+            },
+        },
         opts = {
             auto_refresh = false,
             search_venv_managers = true,
@@ -255,11 +270,11 @@ local plugins = {
         "andweeb/presence.nvim",
         -- event = "VeryLazy",
         opts = {
-            main_image          = "file",
-            neovim_image_text   = "Neovim",
-            auto_update         = true,
+            main_image = "file",
+            neovim_image_text = "Neovim",
+            auto_update = true,
             auto_session_enable = false,
-        }
+        },
     },
     {
         "kdheepak/lazygit.nvim",
@@ -274,22 +289,46 @@ local plugins = {
         event = "VeryLazy",
     },
     {
-        'nvim-treesitter/nvim-treesitter-context',
+        "nvim-treesitter/nvim-treesitter-context",
         event = "VeryLazy",
         opts = {
-            enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-            max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
-            min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+            enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+            max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+            min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
             line_numbers = true,
             multiline_threshold = 20, -- Maximum number of lines to show for a single context
-            trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-            mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+            trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+            mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
             -- Separator between context and content. Should be a single character string, like '-'.
             -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
             separator = nil,
-            zindex = 20,     -- The Z-index of the context window
+            zindex = 20, -- The Z-index of the context window
             on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-        }
-    }
+        },
+    },
+    {
+        "NvChad/nvterm",
+        opts = {
+            terminals = {
+                type_opts = {
+                    float = {
+                        relative = "editor",
+                        row = 0.1,
+                        col = 0.1,
+                        width = 0.8,
+                        height = 0.7,
+                        border = "single",
+                    },
+                },
+            },
+        },
+        init = function()
+            require("core.utils").load_mappings "nvterm"
+        end,
+        config = function(_, opts)
+            require "base46.term"
+            require("nvterm").setup(opts)
+        end,
+    },
 }
 return plugins
