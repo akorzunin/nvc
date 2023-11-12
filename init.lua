b12bd5b3dcbe8cca6318c51da9bac5b0bc9a691f
akorzunin <akorzunin123@gmail.com>
2023-11-12 23:50:46 +0300

    fix: format init and plugins files

--- init.lua
+++ init.lua
@@ -2,17 +2,17 @@ DEBUG = false
 -- DEBUG = true
 require "globals"
 if IS_WINDOWS then
-    vim.api.nvim_exec("language en_US", true)
+  vim.api.nvim_exec("language en_US", true)
 end
 
 -- nvchad configs
 require "core"
 
 local custom_init_path =
-    vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+  vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
 if custom_init_path then
-    dofile(custom_init_path)
+  dofile(custom_init_path)
 end
 
 require("core.utils").load_mappings()
@@ -21,8 +21,8 @@ local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 -- bootstrap lazy.nvim!
 if not vim.loop.fs_stat(lazypath) then
-    require("core.bootstrap").gen_chadrc_template()
-    require("core.bootstrap").lazy(lazypath)
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
 end
 
 dofile(vim.g.base46_cache .. "defaults")
@@ -32,8 +32,8 @@ require "plugins"
 -- TODO: move to apropriate place later
 -- Set breakpon
 vim.fn.sign_define(
-    "DapBreakpoint",
-    { text = "•", texthl = "red", linehl = "", numhl = "" }
+  "DapBreakpoint",
+  { text = "•", texthl = "red", linehl = "", numhl = "" }
 )
 require("dap-go").setup {}
 
@@ -41,4 +41,4 @@ local CMP = require "cmp"
 CMP.mapping["Down"] = CMP.mapping["<Tab>"]
 CMP.mapping["Up"] = CMP.mapping["<S-Tab>"]
 
-require('snippets.lua_snippets')
+require "snippets.lua_snippets"
