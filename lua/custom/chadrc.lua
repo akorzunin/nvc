9a3a5dd4b910fb73f7d39e3ee89c4efee2b7213c
akorzunin <akorzunin123@gmail.com>
2023-11-17 15:00:48 +0300

    fix: always show tabufline bar

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -11,19 +11,20 @@ M.ui = {
   statusline = {
     theme = "default", -- default/vscode/vscode_colored/minimal
     overriden_modules = function(modules)
--- https://github.com/NvChad/ui/blob/e89e67da3237d965ab10230de30071d1fdcf0b02/lua/nvchad/statusline/default.lua
       modules[10] = (function()
         local left_sep = "%#St_pos_sep#" .. "" .. "%#St_pos_icon#" .. " "
-        --
         local current_line = vim.fn.line(".", vim.g.statusline_winid)
         local total_line = vim.fn.line("$", vim.g.statusline_winid)
-        local r,c = unpack(vim.api.nvim_win_get_cursor(0))
-        local text = r .. ':' .. c + 1
+        local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+        local text = r .. ":" .. c + 1
         text = (current_line == 1 and "Top") or text
         text = (current_line == total_line and "Bot") or text
         return left_sep .. "%#St_pos_text#" .. " " .. text .. " "
       end)()
     end,
+  },
+  tabufline = {
+    lazyload = false,
   },
 }
 
